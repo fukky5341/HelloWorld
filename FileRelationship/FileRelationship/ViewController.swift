415fb59c305c430e81e5a1d3c93b7fe48173f5ab
fukky5341 <123881031+fukky5341@users.noreply.github.com>
2023-03-19 01:00:29 +0900

    Add files via upload

---
+++ FileRelationship/FileRelationship/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FileRelationship
+//
+//  Created by fukuda on 2023/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        label1.text = "１つ目の画面"
+    }
+
+
+}
+
---
+++ ScreenTransition/ModalViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ModalViewController.swift
+//  ScreenTransition
+//
+//  Created by fukuda on 2023/03/18.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backActionButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
